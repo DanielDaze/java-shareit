5109786560bf7e0e60051249e85050bde2f4af27
DanielDaze <tattoo255@yandex.ru>
2024-07-20 21:41:43 +0300

    - add UserRepository.java

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,54 +1,6 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.exception.DuplicatedDataException;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.*;
-
-@Repository
-public class UserRepository {
-    long curId = 0;
-    private final Map<Long, User> users = new HashMap<>();
-
-    public Collection<User> getAll() {
-        return users.values();
-    }
-
-    public User get(long id) {
-        return users.get(id);
-    }
-
-    public User create(User user) {
-        List<String> emails = users.values().stream().map(User::getEmail).toList();
-        if (emails.contains(user.getEmail())) {
-            throw new DuplicatedDataException("Пользователь с таким почтовым адресом уже создан");
-        }
-
-        curId++;
-        user.setId(curId);
-        users.put(curId, user);
-        return user;
-    }
-
-    public User update(long id, User user) {
-        List<String> emails = users.values().stream().filter(curUser -> curUser.getId() != id).map(User::getEmail).toList();
-        if (emails.contains(user.getEmail())) {
-            throw new DuplicatedDataException("Пользователь с таким почтовым адресом уже создан");
-        }
-
-        User oldUser = users.get(id);
-        if (user.getName() == null) {
-            user.setName(oldUser.getName());
-        }
-        if (user.getEmail() == null) {
-            user.setEmail(oldUser.getEmail());
-        }
-        user.setId(id);
-        users.replace(id, user);
-        return user;
-    }
-
-    public void delete(long id) {
-        users.remove(id);
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import ru.practicum.shareit.Marker.Create;
+import ru.practicum.shareit.exception.DuplicatedDataException;
+import ru.practicum.shareit.exception.NoSuchDataException;
 
 import java.util.Collection;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -15,23 +18,41 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     public Collection<User> getAll() {
-        return userRepository.getAll();
+        return userRepository.findAll();
     }
 
     public User get(long id) {
-        return userRepository.get(id);
+        return userRepository.findById(id).orElseThrow(NoSuchDataException::new);
     }
 
     @Validated(Create.class)
     public User create(@Valid User user) {
-        return userRepository.create(user);
+        List<String> emails = userRepository.findAll().stream().map(User::getEmail).toList();
+        if (emails.contains(user.getEmail())) {
+            throw new DuplicatedDataException("Пользователь с такой почтой уже зарегистрирован");
+        } else {
+            return userRepository.save(user);
+        }
     }
 
     public User update(long id, User user) {
-        return userRepository.update(id, user);
+        List<String> emails = userRepository.findAll().stream().filter(curUser -> curUser.getId() != id).map(User::getEmail).toList();
+        if (emails.contains(user.getEmail())) {
+            throw new DuplicatedDataException("Пользователь с таким почтовым адресом уже создан");
+        }
+        
+        User foundUser = userRepository.findById(id).orElseThrow(NoSuchDataException::new);
+        if (user.getName() == null) {
+            user.setName(foundUser.getName());
+        }
+        if (user.getEmail() == null) {
+            user.setEmail(foundUser.getEmail());
+        }
+        user.setId(id);
+        return userRepository.save(user);
     }
 
     public void delete(long id) {
-        userRepository.delete(id);
+        userRepository.deleteById(id);
     }
 }
