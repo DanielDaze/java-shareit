5eaa27b0e388ad2b118e694050e96e3f21e6b467
DanielDaze <tattoo255@yandex.ru>
2024-07-26 12:53:49 +0300

    - db fix

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -3,10 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import ru.practicum.shareit.booking.Booking;
 
 public class BookingMapper {
-    public static BookingDto toBookingDto(Booking booking) {
-        return new BookingDto(booking.getStart(), booking.getEnd(), booking.getItem().getId());
-    }
-
     public static Booking toBooking(BookingDto dto) {
         Booking booking = new Booking();
         booking.setStart(dto.getStart());
