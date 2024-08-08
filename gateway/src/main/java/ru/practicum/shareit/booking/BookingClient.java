ae42a8746cad3e45a8846de546b994e9fe0f81d6
DanielDaze <tattoo255@yandex.ru>
2024-08-08 22:51:06 +0300

    - tests coverage WIP

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -3,9 +3,6 @@ package ru.practicum.shareit.booking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
@@ -36,12 +33,10 @@ public class BookingClient extends BaseClient {
     }
 
     public ResponseEntity<Object> approve(Long userId, Long bookingId, Boolean approved) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Sharer-User-Id", userId.toString());
-
-        HttpEntity<Object> entity = new HttpEntity<>("body", headers);
-
-        return rest.exchange(bookingId + "?approved=" + approved, HttpMethod.PATCH, entity, Object.class);
+        Map<String, Object> parameters = Map.of(
+                "approved", approved
+        );
+        return patch("/" + bookingId + "?approved={approved}", userId, parameters);
     }
 
     public ResponseEntity<Object> get(long bookingId, long userId) {
