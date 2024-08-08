b0f5c1023463faec3b02afbd41fa5a44db8ae54f
DanielDaze <tattoo255@yandex.ru>
2024-08-08 16:44:38 +0300

    - Request Logic Tests DONE

--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -10,8 +10,6 @@ import ru.practicum.shareit.Marker;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
-import java.util.Collection;
-
 /**
  * TODO Sprint add-controllers.
  */
